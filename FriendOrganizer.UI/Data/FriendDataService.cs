262d8a917dafaeef89aa8c3bd7606e267177faad
kchungsemchong <kchungsemchong@gmail.com>
2018-09-09 11:47:18 +0400

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,18 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+	public class FriendDataService : IFriendDataService
+	{
+		public IEnumerable<Friend> GetAll()
+		{
+			yield return new Friend { FirstName = "John", LastName = "Smith" };
+			yield return new Friend { FirstName = "Sara", LastName = "Parker" };
+		}
+	}
+}
---
+++ FriendOrganizer.UI/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FriendOrganizer.UI.ViewModel;
+using System;
+using System.Windows;
+
+namespace FriendOrganizer.UI
+{
+	public partial class MainWindow : Window
+	{
+		private MainViewModel _viewModel;
+		public MainWindow(MainViewModel viewModel)
+		{
+			InitializeComponent();
+			_viewModel = viewModel;
+			DataContext = _viewModel;
+			Loaded += MainViewModel_Loaded;
+		}
+
+		private void MainViewModel_Loaded(object sender, RoutedEventArgs e)
+		{
+			_viewModel.Load();
+		}
+	}
+}
